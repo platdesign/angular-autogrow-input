9ee882574f4c13fb21bb3aaca3f95ad336688832
Christian Blaschke <mail@platdesign.de>
2015-03-04 03:12:15 +0100

    debugging for safari and firefox

--- src/autogrow.js
+++ src/autogrow.js
@@ -13,71 +13,73 @@ try {
 module.exports = ngModule;
 
 
-ngModule.directive('pdAutogrowInput', function() {
+ngModule.directive('pdAutogrowInput', [function() {
 
 	return {
 		restrict: 'A',
 		scope: { model: '=ngModel' },
-		link: function postLink(scope, element, attrs) {
-			var wrapper = angular.element('<div style="position:fixed; top:-999px; left:0;"></div>').appendTo(angular.element('body'));
-			var mirror = angular.element('<span style="white-space:pre;"></span>').appendTo(wrapper);
+		priority: 10000,
+		compile: function() {
+			return {
+				post: function(scope, element, attrs) {
+					var wrapper = angular.element('<div style="position:fixed; top:-100px; left:-999px;"></div>').appendTo(angular.element('body'));
+					var mirror = angular.element('<span style="white-space:pre;"></span>').appendTo(wrapper);
 
 
-			var defaultMaxWidth = (element.css('maxWidth') === 'none') ? element.parent().innerWidth() : element.css('maxWidth');
+					var defaultMaxWidth = (element.css('maxWidth') === 'none') ? element.parent().innerWidth() : element.css('maxWidth');
 
-			element.css('minWidth', attrs.puElasticInputMinwidth || element.css('minWidth'));
-			element.css('maxWidth', attrs.puElasticInputMaxwidth || defaultMaxWidth);
-
-			function update() {
-
-				angular.forEach([
-					'fontFamily',
-					'fontSize',
-					'fontWeight',
-					'fontStyle',
-					'letterSpacing',
-					'textTransform',
-					'wordSpacing',
-					'textIndent',
-					'boxSizing',
-					'borderRightWidth',
-					'borderLeftWidth',
-					'borderLeftStyle',
-					'borderRightStyle',
-					'paddingLeft',
-					'paddingRight',
-					'marginLeft',
-					'marginRight'
-				], function (value) {
-					mirror.css(value, element.css(value));
-				});
-
-				mirror.text(element.val() || attrs.placeholder);
-				element.css('width', mirror.outerWidth() + 1);
-
-			}
+					element.css('minWidth', attrs.puElasticInputMinwidth || element.css('minWidth'));
+					element.css('maxWidth', attrs.puElasticInputMaxwidth || defaultMaxWidth);
 
 
-			scope.$watch('model', function (a, b) {
-				update();
-			});
 
-			element.on('keydown keyup focus input propertychange change', function () {
-				update();
-			});
+					function update() {
+
+							angular.forEach([
+								'fontFamily',
+								'fontSize',
+								'fontWeight',
+								'fontStyle',
+								'letterSpacing',
+								'textTransform',
+								'wordSpacing',
+								'textIndent',
+								'boxSizing',
+								'borderRightWidth',
+								'borderLeftWidth',
+								'borderLeftStyle',
+								'borderRightStyle',
+								'paddingLeft',
+								'paddingRight',
+								'marginLeft',
+								'marginRight'
+							], function (value) {
+								mirror.css(value, element.css(value));
+							});
+
+							mirror.text(element.val() || attrs.placeholder);
+							element.css('width', mirror.outerWidth() + 1);
+
+					}
 
 
-			scope.$on('pdAutogrowInputUpdate', function() {
-				setTimeout(function() {
-					update();
-				});
-			});
+					scope.$watch('model', function () {
+						update();
+					});
 
-			// Initial update
-			setTimeout(function() {
-				update();
-			});
-	  }
+					element.on('keydown keyup focus input propertychange change', function () {
+						update();
+					});
+
+					// Initial update
+					setTimeout(function(){
+						update();
+					}, 10);
+				}
+			};
+		}
+
+
 	};
 
-});
+}]);
